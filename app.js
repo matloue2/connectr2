58a70cb6bd47a3e8210b738d57136cdce5d64016
Itumeleng Matlou <matloue2@gmail.com>
2024-03-15 01:18:29 +0200

    added responsiveness using media queries and scripts

--- app.js
+++ app.js
@@ -11,32 +11,59 @@ $(document).ready(function(){
       }
     });
   });
+
+//smooth scroll
+var navbarHeight = $(".navbar").outerHeight();
+$(".navbar-menu a").click(function(e){
+  var targetHref = $(this).attr("href");
+  $("html,body").animate({
+      scrollTop: $(targetHref).offset().top - navbarHeight
+  }, 1000)
+  e.preventDefault();
+})
+
+// navbar mobile version
+const mobile = document.querySelector(".menu-toggle");
+const mobileLink = document.querySelector(".navbar-menu");
+
+mobile.addEventListener("click", function(){
+  mobile.classList.toggle("is-active");
+  mobileLink.classList.toggle("active");
+})
+
+mobileLink.addEventListener("click", function(){
+  const menuBars = document.querySelector(".is-active");
+  if(window.innerWidth <=768 && menuBars) {
+    mobile.classList.toggle("is-active");
+    mobileLink.classList.remove("active");
+  }
+})
 // swiper
 var swiper = new Swiper(".mySwiper",
 {
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disablOnIntereraction: false,
-    },
-    slidePerView:1,
-    spaceBetween:10, 
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-    },
-    breakpoints:{
-        640:{
-            slidesPerView: 2,
-            spaceBetween:20,
-        },
-        768:{
-            slidesPerView: 3,
-            spaceBetween: 40,
-        },
-        1024: {
-            slidesPerview:3,
-            spaceBetween:50,
-        }
-    }
+  loop: true,
+  autoplay: {
+      delay: 2500,
+      disablOnIntereraction: false,
+  },
+  slidePerView:1,
+  spaceBetween:10, 
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+  },
+  breakpoints:{
+      640:{
+          slidesPerView: 2,
+          spaceBetween:20,
+      },
+      768:{
+          slidesPerView: 3,
+          spaceBetween: 40,
+      },
+      1024: {
+          slidesPerview:3,
+          spaceBetween:50,
+      }
+  }
 })
