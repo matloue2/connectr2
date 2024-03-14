a5b8f0265406b63799cf4699e4165fe11e7e01ea
Itumeleng Matlou <matloue2@gmail.com>
2024-03-14 12:06:45 +0200

    I added blog, and footer sections, also worked on testimonials swipper, and added a navbar function that adds a black backround style when scrolling into a white area of the application

---
+++ app.js
@@ -0,0 +1,42 @@
+// change navbar color
+$(document).ready(function(){
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll > 580) {
+        $(".navbar").css("background", "#222");
+        $(".navbar").css("box-shadow", "rgba(0, 0, 0, 0.1) 0px 4px 12px");
+      } else {
+        $(".navbar").css("background", "transparent");
+        $(".navbar").css("box-shadow", "none"); // Corrected line
+      }
+    });
+  });
+// swiper
+var swiper = new Swiper(".mySwiper",
+{
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disablOnIntereraction: false,
+    },
+    slidePerView:1,
+    spaceBetween:10, 
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 2,
+            spaceBetween:20,
+        },
+        768:{
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerview:3,
+            spaceBetween:50,
+        }
+    }
+})
