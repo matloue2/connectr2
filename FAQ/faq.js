6b1223849af4787f462dc487b20a0ab066ea4d87
Itumeleng Matlou <matloue2@gmail.com>
2024-03-27 22:30:40 +0200

    I added a FAQ page

---
+++ FAQ/faq.js
@@ -0,0 +1,18 @@
+const acc = document.getElementsByClassName("accordion");
+
+let i;
+
+for(i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        this.parentElement.classList.toggle("active");
+
+        let pannel = this.nextElementSibling;
+
+        if(pannel.style.display === "block") {
+            pannel.style.display = "none";
+        } else {
+            pannel.style.display = "block";
+        }
+    });
+}
